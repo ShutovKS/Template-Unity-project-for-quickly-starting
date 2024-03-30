030d2c52a0ea49ba31c36957b4c88e0c5512384f
ShutovKS <schutow.kir@yandex.ru>
2024-03-30 22:52:20 +0300

    Add corutine runer service

---
+++ src/Template project/Assets/!ProjectFiles/Scripts/Services/CoroutineRunner/ICoroutineRunner.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Services.CoroutineRunner
+{
+    public interface ICoroutineRunner
+    {
+        public Coroutine StartCoroutine(IEnumerator coroutine);
+    }
+}
